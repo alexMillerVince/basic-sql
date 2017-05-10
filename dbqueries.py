b94858cefe9d35b66256c52a465507d246f18063
alexMillerVince <alex.miller.vince@gmail.com>
2017-05-10 21:13:07 +0200

    set skeleton

---
+++ dbqueries.py
@@ -0,0 +1,27 @@
+from conenct import *
+
+
+def get_name_of_mentors():
+    """
+    returns the 2 name columns of the mentors table.
+    :return: first_name, last_name
+    """
+    pass
+
+
+def get_mentor_by_city(city):
+    """
+    returns the nick_name-s of all mentors working at a specific city.
+    :param city: city name
+    :return: nick_name
+    """
+    pass
+
+
+def get_full_name(first_name):
+    """
+    returns the full_name-s and phone_number-s of an applicant by their first_name
+    :param first_name: the first name of an applicant
+    :return: full_name, phone_number
+    """
+    pass
